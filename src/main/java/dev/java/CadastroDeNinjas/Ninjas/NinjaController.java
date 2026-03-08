0e8fe90471290b0db0a185f7066c0e3e3b07464a
vineog23-boop <vine.og23@gmail.com>
2026-03-08 18:24:35 -0300

    feat: adiciona endpoints CRUD ao NinjaController e corrige artifactId

--- src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,7 +9,34 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas (){
         return "Essa é minha primeira mensagem nessa rota";
+    }
 
+    //Adicionar Ninja      (CREATE)
+    @PostMapping ("/criar")
+    public  String criarNinja(){
+        return "Ninja criado com sucesso";
+    }
+
+    // MOSTRAR TODOS OS NINJAS    (READ)
+@GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrar todos os ninjas";
+    }
+
+    // MOSTRAR  O NINJA POR ID     (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Mostrar o ninja por Id";
+    }
+    // ALTERAR DADOS DO NINJA     (UPDATE)
+@PutMapping ("/alterarID")
+    public String alterarNinjaPorId(){
+        return "Altererar Ninja por Id";
+    }
+    // DELETAR NINJA            (DELETE)
+    @DeleteMapping ("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Ninja deletado por Id";
     }
 
 }
